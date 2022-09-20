db1683f097f6e9a5a1a6c0e94c0cbde2747480f7
Olha <olunchik.ivanova@gmail.com>
2022-09-20 21:33:40 +0100

    temperature changes

--- src/index.js
+++ src/index.js
@@ -112,11 +112,11 @@ function convertToCelsius(event) {
   temperatureElement.innerHTML = 19;
 }
 
-let fahrenheitLink = document.querySelector("#fahrenheit-link");
-fahrenheitLink.addEventListener("click", convertToFahrenheit);
+//let fahrenheitLink = document.querySelector("#fahrenheit-link");
+//fahrenheitLink.addEventListener("click", convertToFahrenheit);
 
-let celsiusLink = document.querySelector("#celsius-link");
-celsiusLink.addEventListener("click", convertToCelsius);
+//let celsiusLink = document.querySelector("#celsius-link");
+//celsiusLink.addEventListener("click", convertToCelsius);
 
 //currentlocation
 function showWeather(response) {
